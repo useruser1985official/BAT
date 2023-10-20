2df7364311bae2b02943d5cbae4db3ec84a2b7d7
User User <useruser1985official@outlook.com>
2023-10-20 19:13:59 -0300

    Atualização Semanal

--- Deletar Temporários.bat
+++ Deletar Temporários.bat
@@ -352,9 +352,9 @@ reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v utweb /f
 
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v MicrosoftEdgeAutoLaunch_AF623091453CC2E58F15E4A702B7C681 /f
 
-:: reg add HKLM\SYSTEM\CurrentControlSet\Services\AdvancedSystemCareService16 /v Start /t REG_DWORD /d 3 /f
+:: reg add HKLM\SYSTEM\CurrentControlSet\Services\AdvancedSystemCareService17 /v Start /t REG_DWORD /d 3 /f
 
-:: reg add HKLM\SYSTEM\CurrentControlSet\Services\AdvancedSystemCareService16 /v DelayedAutoStart /t REG_DWORD /d 1 /f
+:: reg add HKLM\SYSTEM\CurrentControlSet\Services\AdvancedSystemCareService17 /v DelayedAutoStart /t REG_DWORD /d 1 /f
 
 reg add HKLM\SYSTEM\CurrentControlSet\Services\AdobeARMservice /v Start /t REG_DWORD /d 4 /f
 
