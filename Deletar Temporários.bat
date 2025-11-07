e33e60321c6fd183e4420098c5b41e124d8199a1
User User <useruser1985official@outlook.com>
2025-11-07 19:00:26 -0300

    Atualização Semanal

--- Deletar Temporários.bat
+++ Deletar Temporários.bat
@@ -78,7 +78,7 @@ net stop GoogleChromeElevationService
 
 net stop GoogleUpdaterInternalService143.0.7482.0
 
-net stop GoogleUpdaterService142.0.7416.0
+net stop GoogleUpdaterService143.0.7482.0
 
 net stop edgeupdate
 
@@ -424,9 +424,9 @@ reg add HKLM\SYSTEM\CurrentControlSet\Services\gupdatem /v Start /t REG_DWORD /d
 
 reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleChromeElevationService /v Start /t REG_DWORD /d 3 /f
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterInternalService141.0.7376.0 /v Start /t REG_DWORD /d 3 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterInternalService143.0.7482.0 /v Start /t REG_DWORD /d 3 /f
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterService141.0.7376.0 /v Start /t REG_DWORD /d 3 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterService143.0.7482.0 /v Start /t REG_DWORD /d 3 /f
 
 reg add HKLM\SYSTEM\CurrentControlSet\Services\edgeupdate /v Start /t REG_DWORD /d 3 /f
 
--- Limpar Chrome.bat
+++ Limpar Chrome.bat
@@ -30,13 +30,13 @@ net stop gupdatem
 
 net stop GoogleChromeElevationService
 
-net stop GoogleUpdaterInternalService142.0.7416.0
+net stop GoogleUpdaterInternalService143.0.7482.0
 
-net stop GoogleUpdaterService142.0.7416.0
+net stop GoogleUpdaterService143.0.7482.0
 
 sc delete GoogleUpdaterInternalService142.0.7416.0
 
-sc delete GoogleUpdaterService141.0.7376.0
+sc delete GoogleUpdaterService142.0.7416.0
 
 taskkill /f /t /im chrome.exe
 
@@ -62,7 +62,7 @@ reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleChromeElevationService /v S
 
 reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterInternalService143.0.7482.0 /v Start /t REG_DWORD /d 3 /f
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterService142.0.7416.0 /v Start /t REG_DWORD /d 3 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\GoogleUpdaterService143.0.7482.0 /v Start /t REG_DWORD /d 3 /f
 
 cd "%programfiles(x86)%/Google/"
 
